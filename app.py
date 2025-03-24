59391fff021fa9b5d98607eba1a4d721bd6d9d81
Luiz Guilherme Franca <luiz.gfr2002@gmail.com>
2025-03-24 15:46:10 -0300

    Adicionando pastas e arquivos da arquitetura MVC

--- app.py
+++ app.py
@@ -3,20 +3,10 @@
 # rodar a aplicação = 'python app.py'
 
 from flask import Flask, render_template #importando a classe Flask da biblioteca flask
+from controllers import routes
 
 app = Flask(__name__, template_folder = 'views') #atribui a classe Flask a 'app', que agora possui todos seus métodos e atributos
-
-@app.route('/') #decorator atribuindo a função home() a rota principal ('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/games')
-def games():
-    game = {'Título' : 'CS-GO',
-            'Ano' : 2012,
-            'Categoria' : 'FPS Online'}
-    jogadores = ['Pedro', 'João', 'Marcos', 'Maria', 'Diego']
-    return render_template('games.html', game=game, jogadores=jogadores)
+routes.init_app(app)
 
 if __name__ == '__main__':
     app.run(host = 'localhost', port = 5000, debug = True) #modo debug serve para a aplicação ficar sempre atualizada no desenv.
