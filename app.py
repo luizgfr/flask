0249d91721b919ffba089c3d02d1b5212342c7f8
Luiz Guilherme Franca <luiz.gfr2002@gmail.com>
2025-03-31 17:15:00 -0300

    Novas implementações de CRUD

--- app.py
+++ app.py
@@ -4,10 +4,24 @@
 
 from flask import Flask, render_template #importando a classe Flask da biblioteca flask
 from controllers import routes
+from models.database import db, Game
+import os
 
 app = Flask(__name__, template_folder = 'views') #atribui a classe Flask a 'app', que agora possui todos seus métodos e atributos
 routes.init_app(app)
 
+# Permite ler o diretório de um determinado arquivo
+dir = os.path.abspath(os.path.dirname(__file__))
+
+# Passamos o diretório ao SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(dir, 'models/games.sqlite3')
+
 if __name__ == '__main__':
+    
+    db.init_app(app=app)
+    # Verifica no início da aplicação se o BD já existe. Caso contrário ele criará o BD.
+    with app.test_request_context():
+        db.create_all()
+
     app.run(host = 'localhost', port = 5000, debug = True) #modo debug serve para a aplicação ficar sempre atualizada no desenv.
 
--- controllers/routes.py
+++ controllers/routes.py
@@ -1,7 +1,8 @@
 import urllib.request
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 import urllib
 import json
+from models.database import db, Game
 
 jogadores = []
 gamelist = [{'Título' : 'CS-GO', 'Ano' : 2012, 'Categoria' : 'FPS Online'}]
@@ -28,8 +29,52 @@ def init_app(app):
         if request.method == 'POST':
             if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
                 gamelist.append({'Título' : request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
-
+        
         return render_template('cadgames.html', gamelist=gamelist)
+
+    # CRUD - LISTAGEM E CADASTRO
+    @app.route('/estoque', methods=['GET', 'POST'])
+    @app.route('/estoque/delete/<int:id>')
+    def estoque(id=None):
+
+        if id:
+            game = Game.query.get(id)
+            # Deleta o cadastro pela ID
+            db.session.delete(game)
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        # Cadastra um novo jogo
+        if request.method == 'POST':
+            newgame = Game(request.form['titulo'], request.form['ano'], request.form['categoria'],request.form['plataforma'], request.form['preco'], request.form['quantidade'])
+            db.session.add(newgame)
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        else:
+            # Captura o valor de 'page' que foi passado pelo método GET
+            # Define como padrão o valor 1 e o tipo inteiro
+            page = request.args.get('page', 1, type=int)
+            # Valor padrão de registros por página (definimos 3)
+            per_page = 3
+            # Faz um SELECT no banco a partir da pagina informada (page)
+            # Filtrando os registro de 3 em 3 (per_page)
+            games_page = Game.query.paginate(page=page,per_page=per_page)
+            return render_template('estoque.html', gamesestoque=games_page)
+        
+    @app.route('/edit/<int:id>', methods=['GET', 'POST'])
+    def edit(id):
+        g = Game.query.get(id)
+        # Edita o jogo com as informações do formulário
+        if request.method == 'POST':
+            g.titulo = request.form['titulo']
+            g.ano = request.form['ano']
+            g.categoria = request.form['categoria']
+            g.plataforma = request.form['plataforma']
+            g.preco = request.form['preco']
+            g.quantidade = request.form['quantidade']
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        return render_template('editgame.html', g=g)
+
     
     @app.route('/apigames', methods=['GET', 'POST'])
     @app.route('/apigames/<int:id>', methods=['GET', 'POST'])
@@ -42,7 +87,7 @@ def init_app(app):
             ginfo = []
             for g in gamesjson:
                 if g['id'] == id:
-                    gingo = g
+                    ginfo = g
                     break
             if ginfo:
                 return render_template('gameinfo.html', ginfo=ginfo)
