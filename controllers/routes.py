be289b1d6fe98564509ab5034dad09d4e0a42d4e
DSM4 <fatec-dsm4@academico.intra>
2025-04-01 20:34:24 -0300

    Adicionando links para rotas

--- controllers/routes.py
+++ controllers/routes.py
@@ -27,25 +27,6 @@ def init_app(app):
     def home():
         return render_template('index.html')
 
-    @app.route('/games', methods=['GET','POST'])
-    def games():
-        game = gamelist[0]
-
-        if request.method == 'POST':
-            if request.form.get('jogador'):
-                jogadores.append(request.form.get('jogador'))
-
-
-        return render_template('games.html', game=game, jogadores=jogadores)
-    
-    @app.route('/cadgames', methods=['GET','POST'])
-    def cadgames():
-        if request.method == 'POST':
-            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
-                gamelist.append({'Título' : request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
-        
-        return render_template('cadgames.html', gamelist=gamelist)
-
     # CRUD - LISTAGEM E CADASTRO
     @app.route('/estoque', methods=['GET', 'POST'])
     @app.route('/estoque/delete/<int:id>')
